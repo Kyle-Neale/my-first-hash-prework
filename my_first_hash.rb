18f3f8e8bdcfa283615c8ea301e15d0ec175e419
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 13:24:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {"name" => "Kyle"}
   
 end
 
@@ -9,8 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2,  "oil_painting" => 3}
 end
 
 def retrieval
@@ -21,7 +20,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifestp["oil_painting"]
 end
 
 def adding
@@ -31,13 +30,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun_powder"] = 4
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
