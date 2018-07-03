ab906968b81050579ab90c2cb481c25aa589f3c7
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 13:26:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifestp["oil_painting"]
+  shipping_manifest["oil_painting"]
 end
 
 def adding
